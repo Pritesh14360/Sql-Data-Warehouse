267135842925d1bb8b85b298f54fd11cc406d262
Pritesh14360 <priteshpatil14360@gmail.com>
2025-12-07 13:49:03 +0530

    Create etl_log table and modify schema
    Added etl_log table and updated schema with rows_loaded column.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -3,6 +3,18 @@
 CREATE DATABASE DataWarehouse;
 
 USE DataWarehouse;
+CREATE TABLE IF NOT EXISTS etl_log (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    batch_name VARCHAR(100),
+    table_name VARCHAR(100),
+    status VARCHAR(20),
+    message TEXT,
+    start_time DATETIME,
+    end_time DATETIME,
+    duration_seconds INT
+);
+ALTER TABLE datawarehouse.etl_log
+ADD COLUMN rows_loaded INT AFTER status;
 
 CREATE SCHEMA bronze;
 CREATE SCHEMA silver;
